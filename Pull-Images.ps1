95dcaabe5de48ae111ba53ba9ccbf267a96125ef
steve_scherrer <steve_scherrer@yahoo.com>
2022-05-22 20:12:02 -0600

    Initial creation.

---
+++ Pull-Images.ps1
@@ -0,0 +1,7 @@
+# Pull the latest images that are currently store in the local image repository.
+docker pull rabbitmq
+docker pull nextcloud
+docker pull redis
+docker pull mongo
+docker pull kong
+docker pull mongo-express
