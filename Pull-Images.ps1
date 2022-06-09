dc0ba10609243f1fe3fdaa9a08ce1bafd1f7a5bb
steve_scherrer <steve_scherrer@yahoo.com>
2022-06-08 22:03:53 -0600

    Added console output and line breaks.

--- Pull-Images.ps1
+++ Pull-Images.ps1
@@ -1,7 +1,18 @@
 # Pull the latest images that are currently store in the local image repository.
+Write-Output "`nPulling rabbitmq..."
 docker pull rabbitmq
+
+Write-Output "`nPulling nextcloud..."
 docker pull nextcloud
+
+Write-Output "`nPulling redis..."
 docker pull redis
+
+Write-Output "`nPulling mongo..."
 docker pull mongo
+
+Write-Output "`nPulling kong..."
 docker pull kong
+
+Write-Output "`nPulling mongo-express..."
 docker pull mongo-express
