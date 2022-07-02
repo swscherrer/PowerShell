19afcca3e965d4a8bc4e780dc91ae1510257617f
Steve Scherrer <steve_scherrer@yahoo.com>
2022-07-01 20:41:51 -0600

    Added output, using variable.

--- Run-Containers.ps1
+++ Run-Containers.ps1
@@ -1,4 +1,7 @@
 $name = 'nextcloud'
+Write-Output "Stopping $name container."
 docker container stop $name
+Write-Output "Removing $name container."
 docker container rm $name
-docker run --detach --name nextcloud --publish 8080:80 --volume nextcloud:/var/www/html nextcloud
+Write-Output "Running new $name container."
+docker run --detach --name $name --publish 8080:80 --volume nextcloud:/var/www/html $name
