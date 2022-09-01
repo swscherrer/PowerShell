c9c1424dd9883e88f010bd246553e8be955a7a76
Steve Scherrer <steve_scherrer@yahoo.com>
2022-08-31 21:49:55 -0600

    Running redis, OAG-online.

--- Run-Containers.ps1
+++ Run-Containers.ps1
@@ -1,7 +1,24 @@
+
 $name = 'nextcloud'
 Write-Output "Stopping $name container."
 docker container stop $name
 Write-Output "Removing $name container."
 docker container rm $name
-Write-Output "Running new $name container."
-docker run --detach --name $name --publish 8080:80 --volume nextcloud:/var/www/html $name
+Write-Output "Running new $name container"
+docker run --detach --name $name --publish 8180:80 --volume nextcloud:/var/www/html $name
+
+$name = 'redis'
+Write-Output "Stopping $name container."
+docker container stop $name
+Write-Output "Removing $name container."
+docker container rm $name
+Write-Output "Running new $name container"
+docker run --detach --name $name $name
+
+$name = 'openapi-generator-online'
+Write-Output "Stopping $name container."
+docker container stop $name
+Write-Output "Removing $name container."
+docker container rm $name
+Write-Output "Running new $name container"
+docker run --detach --name $name --env GENERATOR_HOST=http://localhost --publish 8181:8080 "openapitools/$($name):latest-release"
