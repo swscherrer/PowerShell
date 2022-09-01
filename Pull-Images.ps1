cd2ca9681a99127d71427afc547870785ef3e47a
Steve Scherrer <steve_scherrer@yahoo.com>
2022-08-31 21:49:42 -0600

    Added tags, OAG-online.

--- Pull-Images.ps1
+++ Pull-Images.ps1
@@ -1,24 +1,27 @@
-# Pull the latest images that are currently store in the local image repository.
+# Pull the latest images that are currently stored in the local image repository.
 Write-Output "`nPulling rabbitmq..."
-docker pull rabbitmq
+docker pull rabbitmq:latest
 
 Write-Output "`nPulling nextcloud..."
-docker pull nextcloud
+docker pull nextcloud:latest
 
 Write-Output "`nPulling redis..."
-docker pull redis
+docker pull redis:latest
 
 Write-Output "`nPulling mongo..."
-docker pull mongo
+docker pull mongo:latest
 
 Write-Output "`nPulling kong..."
-docker pull kong
+docker pull kong:latest
 
 Write-Output "`nPulling mongo-express..."
-docker pull mongo-express
+docker pull mongo-express:latest
 
-Write-Output "`nPulling openapi-generator..."
+Write-Output "`nPulling openapi-generator-cli..."
 docker pull openapitools/openapi-generator-cli:latest-release
+
+Write-Output "`nPulling openapi-generator-online..."
+docker pull openapitools/openapi-generator-online:latest-release
 
 Write-Output "`nPruning images..."
 docker image prune -f
